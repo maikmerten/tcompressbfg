c4be6986c6523e7308d9bb905d48d0ac15156903
maikmerten <maikmerten@76b8c98e-b8cb-272e-fa41-bdd961989f27>
2010-09-08 16:32:24 +0000

    check image dimensions

--- src/de/maikmerten/tcompressbfg/Main.java
+++ src/de/maikmerten/tcompressbfg/Main.java
@@ -18,45 +18,55 @@ import javax.imageio.ImageIO;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws Exception {
-        String inputfilename = args[0];
 
+    public static void main(String[] args) throws Exception {
+        boolean mipmaps = true;
+        String inputfilename = args[0];
         String outputfilename = inputfilename.substring(0, inputfilename.lastIndexOf(".")) + ".dds";
-        System.out.println("Writing to " + outputfilename);
 
         File f = new File(inputfilename);
-        
+
         BufferedImage bimage = ImageIO.read(f);
 
+        if(bimage.getWidth() % 4 != 0 || bimage.getHeight() % 4 != 0) {
+            System.out.println("### Image dimensions are not multiples of four. Aborting. ###");
+            System.exit(1);
+        }
+
+
+        double wlog = Math.log(bimage.getWidth()) / Math.log(2);
+        double hlog = Math.log(bimage.getHeight()) / Math.log(2);
+        if (Math.round(wlog) != wlog || Math.round(hlog) != hlog) {
+            System.out.println("### Image dimensions are not powers of two. Disabling mipmaps. ###");
+            mipmaps = false;
+        }
 
 
         List<BufferedImage> mips = new LinkedList<BufferedImage>();
         mips.add(bimage);
-        int width = bimage.getWidth();
-        int height = bimage.getHeight();
 
-        while (width > 1 || height > 1) {
-            width = width >> 1;
-            height = height >> 1;
+        if (mipmaps) {
+            int width = bimage.getWidth();
+            int height = bimage.getHeight();
 
-            width = Math.max(width, 1);
-            height = Math.max(height, 1);
+            while (width > 1 || height > 1) {
+                width = width >> 1;
+                height = height >> 1;
 
-            BufferedImage mip = new BufferedImage(width, height, bimage.getType());
-            Graphics2D graphics2D = mip.createGraphics();
-            AffineTransform xform = AffineTransform.getScaleInstance((1d * width) / bimage.getWidth(), (1d * height) / bimage.getHeight());
-            graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                    RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-            graphics2D.drawImage(bimage, xform, null);
-            graphics2D.dispose();
+                width = Math.max(width, 1);
+                height = Math.max(height, 1);
 
-            mips.add(mip);
+                BufferedImage mip = new BufferedImage(width, height, bimage.getType());
+                Graphics2D graphics2D = mip.createGraphics();
+                AffineTransform xform = AffineTransform.getScaleInstance((1d * width) / bimage.getWidth(), (1d * height) / bimage.getHeight());
+                graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                        RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+                graphics2D.drawImage(bimage, xform, null);
+                graphics2D.dispose();
+
+                mips.add(mip);
+            }
         }
-
-
 
 
         FileOutputStream fos = new FileOutputStream(new File(outputfilename));
@@ -67,10 +77,11 @@ public class Main {
         header.width = bimage.getWidth();
         header.height = bimage.getHeight();
 
-        if(mips.size() > 1) {
+        if (mips.size() > 1) {
             header.mipmaps = mips.size();
         }
 
+        System.out.println("Writing to " + outputfilename);
         header.writeBytes(dos);
 
 
@@ -79,8 +90,6 @@ public class Main {
         }
 
         fos.close();
-
-
 
     }
 }
